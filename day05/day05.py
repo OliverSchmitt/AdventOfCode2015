86cc14884b33361947799ae2007fdd26273811f4
Oliver Schmitt <oliverschmitt@gmx.de>
2019-04-03 10:04:33 +0200

    Add directory for day05

---
+++ day05/day05.py
@@ -0,0 +1,16 @@
+import sys
+sys.path.append("..")
+import util
+
+def partOne():
+    pass
+
+def partTwo():
+    pass
+
+def main():
+    partOne()
+    partTwo()
+
+if __name__ == "__main__":
+    main()
